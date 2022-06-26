f908b295bdff779697a63372096e812731654f24
Eduardo Santos <eduardosantoshf@gmail.com>
2022-06-26 20:18:11 +0100

    Code refactored

--- prog1/computeDeterminant.cu
+++ prog1/computeDeterminant.cu
@@ -17,9 +17,7 @@ double computeDeterminant(int order,  double *matrix) {
     for (int i = 0; i < order; ++i) {
         pivotElement = matrix[ (i * order) + i]; // current diagonal element
         pivotRow = i;
-        // partial pivoting, which should select
-        // the entry with largest absolute value from the column of the matrix
-        // that is currently being considered as the pivot element
+   
         for (int row = i + 1; row < order; ++row) {
             if (fabs(matrix[(row * order) + i]) > fabs(pivotElement)) {
                 // update the value of the pivot and pivot row index
@@ -27,11 +25,12 @@ double computeDeterminant(int order,  double *matrix) {
                 pivotRow = row;
             }
         }
-        // if the diagonal element is zero then the determinant will be zero
+        
         if (pivotElement == 0.0)
-            return 0.0;
+            return 0.0; // if diagonal is zero => determinant is zero
 
-        if (pivotRow != i) { // if the pivotELement is not in the current row, then we perform a swap in the columns
+        // swap the columns
+        if (pivotRow != i) { 
             for (int k = 0; k < order; k++) {
                 double temp;
                 temp = matrix[(i * order) + k];
@@ -39,15 +38,16 @@ double computeDeterminant(int order,  double *matrix) {
                 matrix[(pivotRow * order) + k] = temp;
             }
 
-            determinant *= -1.0; // signal the row swapping
+            determinant *= -1.0; // because of the swap
         }
 
         determinant *= pivotElement; // update the determinant with the the diagonal value of the current row
 
-        for (int row = i + 1; row < order; ++row) { /* reduce the matrix to a  Upper Triangle Matrix */
+        // reduce the matrix to a upper triangle matrix
+        for (int row = i + 1; row < order; ++row) {
         // as the current row and column "i" will no longer be used, we may start reducing on the next row/column (i+1)
             for (int col = i + 1; col < order; ++col)
-                matrix[(row * order) + col] -= matrix[(row * order) + i] * matrix[(i * order) + col] / pivotElement;  //reduce the value
+                matrix[(row * order) + col] -= matrix[(row * order) + i] * matrix[(i * order) + col] / pivotElement;
         }
     }
 
