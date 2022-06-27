c8059fa4f9b67df704d43390cb28f856b166eb5b
Eduardo Santos <eduardosantoshf@gmail.com>
2022-06-27 01:03:24 +0100

    Project report

--- problem2/dispatcher.c
+++
@@ -1,5 +0,0 @@
-#include "dispatcher.h"
-
-void storePartialResult(double **results, int fileId, int matrixId, double determinant) {
-    results[fileId][matrixId] = determinant; /* store value */
-}
